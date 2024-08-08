02066d7c0a0d63dd87f31f4edf09260bb82fbe06
Julia Zmytrovych <97659424+YuliyaZmytrovych@users.noreply.github.com>
2024-08-08 13:37:19 +0300

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hey, this is my JS program'); 
+
+
+var inputString = "Hello World";
+var resultString = "";
+for (var i = inputString.length - 1; i >= 0; i--)
+{ 
+    resultString += inputString[i];
+}
+console.log(resultString);
+
+function palindrom(str) {
+  var re = /[\W_]/g;
+  var strLow = str.toLowerCase().replace(re, '');
+  var reverseStr = strLow.split('').reverse().join(''); 
+  return reverseStr === strLow;
+}
+var isPal = palindrom("A man, a plan, a canal. Panama");
+if (isPal) {
+   console.log(`strings are equals`);
+} else {
+    console.log(`strings not equal`);
+}
